Cray/Lustre

commit f614fdeee0cd4013112d096b7767e414ac8372c2

LUS-12342 lnet: dynamically configure timeouts
Add/use default LND timeouts:
* SOCKNAL_TIMEOUT_DEFAULT = 50,
* IBLND_TIMEOUT_DEFAULT   = 50,
* KFILND_TIMEOUT_DEFAULT  = 125,
* GNILND_TIMEOUT_BASE 	  = 60

LND timeouts default to these if not set by kernel
module params. Return only this value from the
<lnd>_timeout() functions, dropping the call to
lnet_get_lnd_timeout() which was based on the LTT and
LRC values.

Adds lnd_get_timeout() function to the lnet_lnd API
procedural struct, which returns the LND timeout of
whichever LND initialized the struct.

Use this lnd_get_timeout() function to update
the lnet_lnd tunables upon retrieval, to get current
value from module parameters.

For kfilnd, switch to using kfilnd_timeout() instead of
lnet_get_lnd_timeout(). Define KP_PURGE_LIMIT for KFI
peer purge timeout limits.

For lolnd, there's no timeout function definition, so
added conditional logic to check if the timeout function
is valid and returns a positive integer. Also, LNetGet
using the loopback LND creates the message with both
msg_txni and msg_rxni being NULL, so we check for that
condition.

Use control flow for send/recv to find correct msg NI.
Fix formatting of struct array in nidstrings.c.

Add module param path variables for ksocklnd,
kkfilnd, and kgnilnd. Renames the o2ib_modparam
variable to be more consistent:
o2iblnd_modparam_path.

Remove depency on default lnet_lnd_timeout value
in kgnilnd_timeout() function; use tunable value
instead.

Modifies the 'lnetctl net set' command to allow setting
the LND timeout value via:
'lnetctl net set --net <foo> --lnd-timeout <val>'

Fallback to lnet_get_lnd_timeout() if tunables timeout
value is 0 (or is unset).

Signed-off-by: Caleb Carlson <caleb.carlson@hpe.com>
Test-Parameters: testlist="sanity-lnet"
Change-Id: Ic69a7d9d6af4cfed65d07caaf87d8b78238beab0
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162968
Reviewed-by: Christopher Horn <chris.horn@hpe.com>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -2621,6 +2621,53 @@ int lustre_lnet_config_hsensitivity(int sen, int seq_no, struct cYAML **err_rc)
 	return rc;
 }
 
+int lustre_lnet_config_lnd_timeout(int timeout, __u32 net, int seq_no,
+				   struct cYAML **err_rc)
+{
+	int rc = LUSTRE_CFG_RC_NO_ERR;
+	char err_str[LNET_MAX_STR_LEN] = "";
+	char val[INT_STRING_LEN];
+	__u32 lnd = LNET_NETTYP(net);
+
+	snprintf(val, sizeof(val), "%d", timeout);
+
+	switch (lnd) {
+		case SOCKLND:
+			rc = write_sysfs_file(socklnd_modparam_path,
+					      "sock_timeout", val, 1,
+					      strlen(val) + 1);
+			break;
+		case O2IBLND:
+			rc = write_sysfs_file(o2iblnd_modparam_path,
+					      "timeout", val, 1,
+					      strlen(val) + 1);
+			break;
+		case KFILND:
+			rc = write_sysfs_file(kfilnd_modparam_path,
+					      "kfi_timeout", val, 1,
+					      strlen(val) + 1);
+			break;
+		case GNILND:
+			rc = write_sysfs_file(gnilnd_modparam_path,
+					      "timeout", val, 1,
+					      strlen(val) + 1);
+		default:
+			snprintf(err_str, sizeof(err_str),
+				 "\"Net %s does not accept a LND timeout\"",
+				 libcfs_lnd2str(lnd));
+			rc = -EINVAL;
+	}
+
+	/* Check return code from writing sysfs file */
+	if (rc)
+		snprintf(err_str, sizeof(err_str),
+			 "\"Failed to set LND timeout for net %s\"",
+			 libcfs_lnd2str(lnd));
+
+	cYAML_build_error(rc, seq_no, "set", "lnd_timeout", err_str, err_rc);
+	return rc;
+}
+
 int lustre_lnet_config_transaction_to(int timeout, int seq_no, struct cYAML **err_rc)
 {
 	int rc = LUSTRE_CFG_RC_NO_ERR;
@@ -3719,7 +3766,7 @@ int lustre_lnet_calc_service_id(__u64 *service_id)
 	char val[LNET_MAX_STR_LEN];
 	int service_port = -1, l_errno = 0;
 
-	rc = read_sysfs_file(o2ib_modparam_path, "service", val,
+	rc = read_sysfs_file(o2iblnd_modparam_path, "service", val,
 			     1, sizeof(val));
 	if (rc) {
 		l_errno = errno;

lnet/include/lnet/lib-types.h
@@ -327,6 +327,9 @@ struct lnet_lnd {
 	/* get dma_dev priority */
 	unsigned int (*lnd_get_dev_prio)(struct lnet_ni *ni,
 					 unsigned int dev_idx);
+
+	/* get LND timeout */
+	int (*lnd_get_timeout)(void);
 };
 
 struct lnet_tx_queue {

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -108,6 +108,7 @@ struct lnet_ioctl_config_kfilnd_tunables {
 	__u32 lnd_auth_key;
 	char lnd_traffic_class_str[LNET_MAX_STR_LEN];
 	__u32 lnd_traffic_class;
+	__u32 lnd_timeout;
 };
 
 struct lnet_ioctl_config_socklnd_tunables {

lnet/klnds/gnilnd/gnilnd.c
@@ -33,6 +33,7 @@ const struct lnet_lnd the_kgnilnd = {
 	.lnd_send       = kgnilnd_send,
 	.lnd_recv       = kgnilnd_recv,
 	.lnd_eager_recv = kgnilnd_eager_recv,
+	.lnd_get_timeout = kgnilnd_timeout,
 };
 
 kgn_data_t      kgnilnd_data;

lnet/klnds/gnilnd/gnilnd.h
@@ -900,9 +900,7 @@ extern int _kgnilnd_schedule_delay_conn(kgn_conn_t *conn);
 
 static inline int kgnilnd_timeout(void)
 {
-	return *kgnilnd_tunables.kgn_timeout ?
-	       *kgnilnd_tunables.kgn_timeout :
-	       lnet_get_lnd_timeout();
+	return *kgnilnd_tunables.kgn_timeout ?: lnet_get_lnd_timeout();
 }
 
 /* Macro wrapper for _kgnilnd_schedule_conn. This will store the function

lnet/klnds/kfilnd/kfilnd.c
@@ -361,11 +361,12 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 static int kfilnd_startup(struct lnet_ni *ni);
 
 static const struct lnet_lnd the_kfilnd = {
-	.lnd_type	= KFILND,
-	.lnd_startup	= kfilnd_startup,
-	.lnd_shutdown	= kfilnd_shutdown,
-	.lnd_send	= kfilnd_send,
-	.lnd_recv	= kfilnd_recv,
+	.lnd_type		= KFILND,
+	.lnd_startup		= kfilnd_startup,
+	.lnd_shutdown		= kfilnd_shutdown,
+	.lnd_send		= kfilnd_send,
+	.lnd_recv		= kfilnd_recv,
+	.lnd_get_timeout	= kfilnd_timeout,
 };
 
 static int kfilnd_startup(struct lnet_ni *ni)

lnet/klnds/kfilnd/kfilnd.h
@@ -116,6 +116,9 @@
 
 #define KFILND_MY_PROCID 49152
 
+/* default kfilnd timeout in seconds */
+#define KFILND_TIMEOUT_DEFAULT 125
+
 /* 256 Rx contexts max */
 #define KFILND_FAB_RX_CTX_BITS 8
 
@@ -143,6 +146,7 @@ extern const struct file_operations kfilnd_reset_stats_file_ops;
 
 extern struct workqueue_struct *kfilnd_wq;
 
+extern int kfi_timeout;
 extern unsigned int cksum;
 extern unsigned int tx_scale_factor;
 extern unsigned int rx_cq_scale_factor;
@@ -260,6 +264,11 @@ static inline bool kfilnd_peer_deleted(struct kfilnd_peer *kp)
 	return atomic_read(&kp->kp_remove_peer) > 0;
 }
 
+static inline int kfilnd_timeout(void)
+{
+	return kfi_timeout ?: lnet_get_lnd_timeout();
+}
+
 /* Values for kp_hello_state. Valid transitions:
  * NONE -> INIT
  * INIT -> NONE (only when fail to allocate kfilnd_tn for hello req)
@@ -309,11 +318,11 @@ static inline bool kfilnd_peer_needs_hello(struct kfilnd_peer *kp,
 			return true;
 		else if (proactive_handshake &&
 			 ktime_before(kp->kp_last_alive +
-				      lnet_get_lnd_timeout() * 2,
+				      kfilnd_timeout() * 2,
 				      ktime_get_seconds()))
 			return true;
 	} else if (hello_state == KP_HELLO_SENDING &&
-		   ktime_before(kp->kp_hello_ts + lnet_get_lnd_timeout(),
+		   ktime_before(kp->kp_hello_ts + kfilnd_timeout(),
 				ktime_get_seconds())) {
 		/* Sent hello but never received reply */
 		CDEBUG(D_NET,

lnet/klnds/kfilnd/kfilnd_modparams.c
@@ -35,6 +35,10 @@ unsigned int cksum;
 module_param(cksum, uint, 0444);
 MODULE_PARM_DESC(cksum, "Enable checksums for non-zero messages (not RDMA)");
 
+int kfi_timeout = KFILND_TIMEOUT_DEFAULT;
+module_param(kfi_timeout, int, 0644);
+MODULE_PARM_DESC(kfi_timeout, "Timeout (seconds)");
+
 /* Scale factor for TX context queue depth. The factor is applied to the number
  * of credits to determine queue depth.
  */
@@ -214,6 +218,8 @@ int kfilnd_tunables_setup(struct lnet_ni *ni)
 		       kfilnd_tunables->lnd_traffic_class_str);
 		return -EINVAL;
 	}
+
+	kfilnd_tunables->lnd_timeout = kfilnd_timeout();
 
 	return 0;
 }

commit 283f6a57d9e60ad4c9c2ac094021e5800c0691c8

LUS-12679 lnet: fix missing break statement
This fixes a missing 'break' statement for the
GNILND switch case for configuring the LND timeout
module parameter via lnetctl. Otherwise, it would
just fallthrough to the 'default' case, giving the
user an error back even though their change succeeded.

Fixes: f614fdeee0 ("LUS-12342 lnet: dynamically configure timeouts")
Signed-off-by: Caleb Carlson <caleb.carlson@hpe.com>
Change-Id: I62aec70d09078b752d57828e2eae1e7dc6b58abd
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163162
Reviewed-by: Christopher Horn <chris.horn@hpe.com>
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Sakib Samar <sakib.samar@hpe.com>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -2651,6 +2651,7 @@ int lustre_lnet_config_lnd_timeout(int timeout, __u32 net, int seq_no,
 			rc = write_sysfs_file(gnilnd_modparam_path,
 					      "timeout", val, 1,
 					      strlen(val) + 1);
+			break;
 		default:
 			snprintf(err_str, sizeof(err_str),
 				 "\"Net %s does not accept a LND timeout\"",